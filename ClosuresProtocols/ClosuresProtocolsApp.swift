0883c07f2c9f152993fc30bbd49880d18b454629
David Devlin <david.devlin@theverygroup.com>
2023-01-18 20:22:26 +0000

    Initial Commit

---
+++ ClosuresProtocols/ClosuresProtocolsApp.swift
@@ -0,0 +1,16 @@
+//
+// Created 18/01/2023
+// Copyright © The Very Group 2023. All rights reserved.
+//
+        
+
+import SwiftUI
+
+@main
+struct ClosuresProtocolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClosuresProtocols/ContentView.swift
@@ -0,0 +1,25 @@
+//
+// Created 18/01/2023
+// Copyright © The Very Group 2023. All rights reserved.
+//
+        
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
